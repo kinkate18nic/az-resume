9e4815158892f5a6468d6487e4c7dd7c43f1e4a2
Nishanth <kinkate18nic@gmail.com>
2025-03-25 11:47:53 +0530

    Removed function key

--- backend/api/AzureResumeCounter.cs
+++ backend/api/AzureResumeCounter.cs
@@ -24,8 +24,6 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id="1", PartitionKey="1")] Counter counter,
             [CosmosDB(databaseName:"AzureResume", containerName: "Counter", Connection = "AzureResumeConnectionString", Id="1", PartitionKey="1")] out Counter updatedCounter,
-
-
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -39,6 +37,28 @@ namespace Company.Function
             {
                 Content = new StringContent(jsonReturn, Encoding.UTF8, "application/json")
             };
+        }
+
+        [FunctionName("GetFunctionApiUrl")]
+        public static IActionResult GetFunctionApiUrl(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetFunctionApiUrl")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Fetching Function API URL securely.");
+
+            // Retrieve the function key from environment variables
+            string functionKey = Environment.GetEnvironmentVariable("AZUREFUNCTIONKEY");
+
+            if (string.IsNullOrEmpty(functionKey))
+            {
+                log.LogError("Function Key not found.");
+                return new BadRequestObjectResult("Function Key not found.");
+            }
+
+            // Construct the full API URL
+            string functionApiUrl = $"https://getresumecounternish.azurewebsites.net/api/AzureResumeCounter?code={functionKey}";
+
+            return new OkObjectResult(new { url = functionApiUrl });
         }
     }
 }
